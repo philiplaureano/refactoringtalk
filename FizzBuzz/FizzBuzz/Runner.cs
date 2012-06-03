86d221e4af74c828cd5d07e8da889376a6446d69
Philip Laureano <philip.laureano@gmail.com>
2012-06-03 16:35:54 +1000

    Extracted Number Printing methods into separate class

--- FizzBuzz/FizzBuzz/Runner.cs
+++ FizzBuzz/FizzBuzz/Runner.cs
@@ -15,36 +15,5 @@ namespace FizzBuzz
                 PrintNumber(i);
             }
         }
-
-        #region Number Printing Methods
-        private static void PrintNumber(int i)
-        {
-            var whatToPrint = GetOutputString(i);
-
-            Console.WriteLine(whatToPrint);
-        }
-
-        private static string GetOutputString(int i)
-        {
-            var isFizz = i % 3 == 0;
-            var isBuzz = i % 5 == 0;
-
-            if (isFizz && isBuzz)
-                return "FizzBuzz";
-
-            if (isFizz)
-                return "Fizz";
-
-            if (isBuzz)
-                return "Buzz";
-
-            return DisplayTheActualNumber(i);
-        }
-
-        private static string DisplayTheActualNumber(int i)
-        {
-            return i.ToString(CultureInfo.InvariantCulture);
-        } 
-        #endregion
     }
 }
