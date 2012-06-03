06a732db66d1e301c8cd56cb8747c529715ed51a
Philip Laureano <philip.laureano@gmail.com>
2012-06-03 14:52:27 +1000

    Moved the FizzBuzz implementation into another class, but left the program in a broken state to show some Resharper design techniques

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -12,40 +12,5 @@ namespace FizzBuzz
         {
             Run();
         }
-
-        #region The FizzBuzz Implementation
-        private static void Run()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                var whatToPrint = GetOutputString(i);
-
-                Console.WriteLine(whatToPrint);
-            }
-        }
-
-        
-        private static string GetOutputString(int i)
-        {
-            var isFizz = i % 3 == 0;
-            var isBuzz = i % 5 == 0;
-
-            if (isFizz && isBuzz)
-                return "FizzBuzz";
-
-            if (isFizz)
-                return "Fizz";
-
-            if (isBuzz)
-                return "Buzz";
-
-            return DisplayTheActualNumber(i);
-        }
-
-        private static string DisplayTheActualNumber(int i)
-        {
-            return i.ToString(CultureInfo.InvariantCulture);
-        } 
-        #endregion
     }
 }
